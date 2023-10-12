f8cfe884905a232b59bec2e547faeab88707c53e
Kevin Chen <kevchen21@gmail.com>
2023-10-11 21:19:58 -0700

    get expense handler

---
+++ src/functions/expense/get/handler.ts
@@ -0,0 +1,34 @@
+import schema from "./schema";
+import { middyfy } from "../../../libs/lambda";
+import { container } from "../../../di/inversify.config";
+import { IExpenseService } from "../../../services/expense-service/expense-service-interface";
+import { HttpStatusCode, IExpense, DataResponse } from "@splitsies/shared-models";
+import { SplitsiesFunctionHandlerFactory, ILogger, ExpectedError, IExpectedError } from "@splitsies/utils";
+import { UnauthorizedUserError } from "src/models/error/unauthorized-user-error";
+
+const logger = container.get<ILogger>(ILogger);
+const expenseService = container.get<IExpenseService>(IExpenseService);
+
+const expectedErrors: IExpectedError[] = [
+    new ExpectedError(UnauthorizedUserError, HttpStatusCode.FORBIDDEN, "Unauthorized to access this resource"),
+];
+
+export const main = middyfy(
+    SplitsiesFunctionHandlerFactory.create<typeof schema, IExpense | string>(
+        logger,
+        async (event) => {
+            const expenseId = event.pathParameters.expenseId;
+            const userId = event.requestContext.authorizer.userId;
+
+
+            console.log({ expenseId, userId });
+            const result = await expenseService.getUserExpense(userId, expenseId);
+            if (!result) { throw new UnauthorizedUserError(); }
+
+
+            const expense = await expenseService.getExpense(result.expenseId);
+            return new DataResponse(HttpStatusCode.OK, expense).toJson();
+        },
+        expectedErrors,
+    ),
+);
