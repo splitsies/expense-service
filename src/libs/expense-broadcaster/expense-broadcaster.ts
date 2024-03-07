90a62b929715bb1c743d843ac15377bd274d09eb
Kevin Chen <kevchen21@gmail.com>
2024-03-07 08:57:28 -0500

    cleanup expense broadcaster

--- src/libs/expense-broadcaster/expense-broadcaster.ts
+++ src/libs/expense-broadcaster/expense-broadcaster.ts
@@ -4,27 +4,25 @@ import { IExpenseMessage } from "@splitsies/shared-models";
 import { IConnectionConfiguration } from "src/models/configuration/connection/connection-configuration-interface";
 import { IConnectionService } from "src/services/connection-service/connection-service-interface";
 import { sendMessage } from "@libs/broadcast";
+import { ILogger } from "@splitsies/utils";
 
 @injectable()
 export class ExpenseBroadcaster implements IExpenseBroadcaster {
     constructor(
+        @inject(ILogger) private readonly _logger: ILogger,
         @inject(IConnectionConfiguration) private readonly _connectionConfiguration: IConnectionConfiguration,
         @inject(IConnectionService) private readonly _connectionService: IConnectionService,
     ) {}
 
     async broadcast(expenseId: string, message: IExpenseMessage): Promise<void> {
-        console.log("broadcasting...");
-        this._connectionConfiguration.gatewayUrl;
         const connectionIds = await this._connectionService.getConnectionsForExpenseId(expenseId);
 
         const promises: Promise<void>[] = [];
         for (const id of connectionIds) {
-            console.log({ id });
-
             try {
                 promises.push(sendMessage(this._connectionConfiguration.gatewayUrl, id, message));
             } catch (e) {
-                console.log(`the error is ${e}`);
+                this._logger.error(`uncaught exception broadcasting for connection ${id}`, e);
             }
         }
 
