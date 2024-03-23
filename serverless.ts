474d351a614efdb6ccacd77a690158a0b3a15222
Kevin Chen <kevchen21@gmail.com>
2024-03-23 16:58:40 -0700

    service level vpc

--- serverless.ts
+++ serverless.ts
@@ -55,6 +55,19 @@ const serverlessConfiguration: AWS = {
             ...connectionConfig,
             ...firebaseConfig,
         },
+        vpc: {
+            securityGroupIds: [
+                "sg-e7cc00b2"
+            ],
+            subnetIds: [
+                "subnet-0a5b5a6d",
+                "subnet-e2d4d6be",
+                "subnet-74e93839",
+                "subnet-4ca6f372",
+                "subnet-3bdc2735",
+                "subnet-d7ede1f9"
+            ]
+        }
     },
     // import the function via paths
     functions: {
