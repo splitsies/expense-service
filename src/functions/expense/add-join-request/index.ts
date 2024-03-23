90c021ac669e6be65bb0ecf0eb8230fe820d819b
Kevin Chen <kevchen21@gmail.com>
2024-03-23 14:09:27 -0700

    deploy to vpc

--- src/functions/expense/add-join-request/index.ts
+++ src/functions/expense/add-join-request/index.ts
@@ -12,4 +12,17 @@ export default {
             },
         },
     ],
+    vpc: {
+        securityGroupIds: [
+            "sg-e7cc00b2"
+        ],
+        subnetIds: [
+            "subnet-0a5b5a6d",
+            "subnet-e2d4d6be",
+            "subnet-74e93839",
+            "subnet-4ca6f372",
+            "subnet-3bdc2735",
+            "subnet-d7ede1f9"
+        ]
+    }
 };
