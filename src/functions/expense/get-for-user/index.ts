fb42ce86695749e152862f946309ceb4068fe8d9
Kevin Chen <kevchen21@gmail.com>
2024-03-26 22:53:30 -0700

    us-east-1b subnet

--- src/functions/expense/get-for-user/index.ts
+++ src/functions/expense/get-for-user/index.ts
@@ -17,11 +17,7 @@ export default {
             "sg-0c856a69027cbbe51"
         ],
         subnetIds: [
-            "subnet-01628c50fc43d2a65",
-            "subnet-073d80ceea6995302",
             "subnet-0ca030cb2990146fd",
-            "subnet-058fb9347a6e48a46",
-            "subnet-0d9647cee3c8a8a54"
         ]
     }
 };
