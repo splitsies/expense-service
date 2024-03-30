869dbc24e0b0bad385dde8c30eafe6a0674e940f
Kevin Chen <kevchen21@gmail.com>
2024-03-29 17:24:44 -0700

    correct the subnet id

--- src/functions/expense/merge-guest-user/index.ts
+++ src/functions/expense/merge-guest-user/index.ts
@@ -24,7 +24,7 @@ export default {
             "sg-0f8a62286187fbab0"
         ],
         subnetIds: [
-            "subnet-0ca030cb2990146fd",
+            "subnet-0301a21d6a9ca2e03",
         ]
     }
 };
