fd14722f364b1c523a59821e2b8fdb43eced7f42
Kevin Chen <kevchen21@gmail.com>
2024-04-03 10:38:43 -0700

    cleanup (#41)

--- src/models/configuration/db/db-configuration-interface.ts
+++ src/models/configuration/db/db-configuration-interface.ts
@@ -4,9 +4,7 @@ export interface IDbConfiguration {
     readonly dbRegion: string;
     readonly tableName: string;
     readonly expenseItemTableName: string;
-    readonly transactionDateIndexName: string;
     readonly connectionTableName: string;
-    readonly userExpenseTableName: string;
     readonly expenseJoinRequestTableName: string;
     readonly endpoint: string;
     readonly pgHost: string;
