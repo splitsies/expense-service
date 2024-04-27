3245a9f8bd3ad247a1a9e9f1264fbadaed845e07
Kevin Chen <kevchen21@gmail.com>
2024-04-26 21:00:37 -0700

    join request notification

--- src/config/queue.config.ts
+++ src/config/queue.config.ts
@@ -2,4 +2,5 @@ export const QueueConfig = {
     expenseUpdate: "ACB24FB8-B684-4427-8E1A-A2138AB38E95",
     guestUserReplaced: "96152C61-90BC-4A0B-93E0-9B1CA0CA7919",
     userDeleted: "DEA2D24C-9596-4426-A7D6-7102820E51BA",
+    joinRequest: "0B39C186-89FB-4158-9CF3-2FBFBCF2A6B9",
 };
