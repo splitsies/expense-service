08dafaae1761c06f41863fda48eadfdac74476c3
Kevin Chen <kevchen21@gmail.com>
2024-09-22 20:16:13 -0700

    Use SQS

--- src/functions/expense/delete-user-data/index.ts
+++ src/functions/expense/delete-user-data/index.ts
@@ -1,26 +1,12 @@
 import { handlerPath } from "../../../libs/handler-resolver";
-import { QueueConfig } from "src/config/queue.config";
 
 export default {
     handler: `${handlerPath(__dirname)}/handler.main`,
     events: [
         {
-            stream: {
-                type: "dynamodb",
-                arn: "${param:MESSAGE_QUEUE_ARN}",
-                startingPosition: "LATEST",
-                maximumRetryAttempts: 3,
-                maximumRecordAgeInSeconds: 60,
-                filterPatterns: [
-                    {
-                        eventName: ["INSERT"],
-                        dynamodb: {
-                            Keys: { queueName: { S: [QueueConfig.userDeleted] } },
-                            NewImage: { stage: { S: ["${param:QUEUE_STAGE_NAME}"] } },
-                        },
-                    },
-                ],
-            },
+            sqs: {
+               arn: "arn:aws:sqs:${aws:region}:${aws:accountId}:Splitsies-UserAccountModifiedQueue-${sls:stage}"
+            }
         },
     ],
 };
