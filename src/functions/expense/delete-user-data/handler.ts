6c984eea327a9b6e8625126ccce505c5539bffab
Kevin Chen <kevchen21@gmail.com>
2024-09-22 23:32:35 -0700

    testing sqs lambda

--- src/functions/expense/delete-user-data/handler.ts
+++ src/functions/expense/delete-user-data/handler.ts
@@ -2,38 +2,28 @@ import { container } from "../../../di/inversify.config";
 import { IExpenseService } from "../../../services/expense-service/expense-service-interface";
 import { ExpenseMessage, ExpenseMessageType, IQueueMessage } from "@splitsies/shared-models";
 import { IExpenseBroadcaster } from "@libs/expense-broadcaster/expense-broadcaster-interface";
-import { DynamoDBStreamHandler } from "aws-lambda/trigger/dynamodb-stream";
-import { unmarshall } from "@aws-sdk/util-dynamodb";
-import { AttributeValue } from "@aws-sdk/client-dynamodb";
 import { IMessageQueueClient } from "@splitsies/utils";
+import { SQSHandler } from "aws-lambda";
 
 const expenseService = container.get<IExpenseService>(IExpenseService);
 const expenseBroadcaster = container.get<IExpenseBroadcaster>(IExpenseBroadcaster);
 const messageQueueClient = container.get<IMessageQueueClient>(IMessageQueueClient);
+const queueUrl = `https://sqs.${process.env.RtRegion}.amazonaws.com/${process.env.AwsAccountId}/Splitsies-UserAccountModifiedQueue-${process.env.Stage}`;
 
-export const main: DynamoDBStreamHandler = async (event, context, callback) => {
+export const main: SQSHandler = async (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
 
     const messages: IQueueMessage<string>[] = [];
     const expenseIds = [];
 
     for (const record of event.Records) {
-        if (!record.dynamodb.NewImage) continue;
+        if (!record.body) continue;
 
-        const message = unmarshall(record.dynamodb.NewImage as Record<string, AttributeValue>) as IQueueMessage<string>;
-        messages.push(message);
-        expenseIds.push(...(await expenseService.deleteUserData(message.data)));
-    }
-
-    for (const id of expenseIds) {
-        const expense = await expenseService.getLeadingExpense(id);
-        await expenseBroadcaster.broadcast(
-            new ExpenseMessage({
-                type: ExpenseMessageType.ExpenseDto,
-                connectedExpenseId: expense.id,
-                expenseDto: expense,
-            }),
-        );
+        console.log({ record });
+        
+        const message = JSON.parse(body) as IQueueMessage<string>;
+        // expenseIds.push(...(await expenseService.deleteUserData(message.data)));
+        messages.push(messageQueueClient.createDeleteRecord(queueUrl, record.messageId, record.receiptHandle));
     }
 
     await messageQueueClient.deleteBatch(messages);
